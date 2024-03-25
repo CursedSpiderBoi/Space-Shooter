f1bdd4e8c3350ea6ff1970171f238210d2efab66
Muhammad Ali Zahid <mohammad.aliz.gg@gmail.com>
2024-03-26 02:17:08 +0500

    we do work

--- components/move_input_component.gd
+++ components/move_input_component.gd
@@ -1,8 +1,9 @@
 class_name MoveInputComponent
 extends Node
 
+@export var move_stats: MoveStats
 @export var move_component: MoveComponent
 
 func _input(event):
 	var input_axis = Input.get_axis("ui_left","ui_right") # returns a value between -1,1 depending upon pressed button
-	move_component.velocity=Vector2(input_axis*50,0)  
+	move_component.velocity=Vector2(move_stats.speed*input_axis,0)  
